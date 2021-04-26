8c1f79a678a928c121b165e89f1e60b9b90fa2b0
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-26 14:07:12 -0500

    refactor user.password setter

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
-    include BCrypt
-    validates :username, presence: true, uniqueness: true
-    validates :password, presence: true
+  include BCrypt
+  validates :username, presence: true, uniqueness: true
+  validates :password, presence: true
 
-    def password
-        @password ||= Password.new(encrypted_password) if encrypted_password.present?
+  def password
+    @password ||= Password.new(encrypted_password) if encrypted_password.present?
+  end
+
+  def password=(new_password)
+    if new_password.blank?
+      @password = new_password
+    else
+      @password = Password.create(new_password)
+      self.encrypted_password = @password
     end
-
-    def password=(new_password)
-        @password = new_password
-        return @password if new_password.blank?
-
-        @password =  Password.create(new_password)
-        self.encrypted_password = @password
-    end
+  end
 end
