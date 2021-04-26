e773794f985d6a81f653dba6f71e902bce2b9eac
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-26 16:47:19 -0500

    add route for sign up funtionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
+  post 'sign_up', to: 'registrations#create'
   resources :books
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
