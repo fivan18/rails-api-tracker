20507e03b660568efe559b960aa2ef7b228f42d6
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-26 13:59:23 -0500

    add validations to the user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,17 @@
 class User < ApplicationRecord
+    include BCrypt
+    validates :username, presence: true, uniqueness: true
+    validates :password, presence: true
+
+    def password
+        @password ||= Password.new(encrypted_password) if encrypted_password.present?
+    end
+
+    def password=(new_password)
+        @password = new_password
+        return @password if new_password.blank?
+
+        @password =  Password.create(new_password)
+        self.encrypted_password = @password
+    end
 end
