ae2794e74de3f6c4c9e8a45d4c1417389efa4fd3
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-26 13:30:12 -0500

    add test for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#validations' do
+    it 'should have valid factoy' do
+      user = build :user
+      expect(user).to be_valid
+    end
+
+    it 'should validate presence of username and password' do
+      user = build :user, username: nil, password: nil
+      expect(user).not_to be_valid
+      expect(user.errors.messages[:username]).to include("can't be blank")
+      expect(user.errors.messages[:password]).to include("can't be blank")
+    end
+
+    it 'should validate uniqueness of username' do
+      user = create :user
+      other_user = build :user, username: user.username
+      expect(other_user).not_to be_valid
+      other_user.username = 'newusername'
+      expect(other_user).to be_valid
+    end
+  end
 end
