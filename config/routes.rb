541d040678c56b7307a4d059d96dd04f3a5da05d
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-04-26 21:12:06 -0500

    add routes for login and logout functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  post 'login', to: 'access_tokens#create'
+  delete 'logout', to: 'access_tokens#destroy'
   post 'sign_up', to: 'registrations#create'
   resources :books
 end
