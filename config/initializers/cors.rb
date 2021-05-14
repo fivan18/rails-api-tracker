c8fbc6a0bb3a923effc8476c910cb91a0ed6bab2
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-05-14 11:03:24 -0500

    fix the origin

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://evening-peak-09291.herokuapp.com'
+    origins 'https://chalisthenics-tracker.herokuapp.com/'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
