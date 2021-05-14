b9369c6d2c14f45d504a064b7a91622d9d015a55
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-05-14 10:52:22 -0500

    change origin for cors policy

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'http://localhost:2000'
+    origins 'https://evening-peak-09291.herokuapp.com'
     resource '*',
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
