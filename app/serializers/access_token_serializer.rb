f80c5a24cd25c451c063f579f43d8f32f65e86ff
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-10-08 15:50:23 -0500

    modify serializer for access token

--- app/serializers/access_token_serializer.rb
+++ app/serializers/access_token_serializer.rb
@@ -2,7 +2,5 @@ class AccessTokenSerializer
   include JSONAPI::Serializer
   attributes :token
 
-  belongs_to :user, meta: proc { |access_token, _params|
-    { username: access_token.user.username }
-  }
+  belongs_to :user
 end
