2affefd28c3d0401fe115d98dc510bb17454a959
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-10-08 23:16:26 -0500

    refactor exercises controller and add validation to exercise model

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,33 +1,33 @@
 class ExercisesController < ApplicationController
-  before_action :set_exercise, only: %i[show update destroy]
   before_action :authorize!
+  before_action :routine
+  before_action :set_exercise, only: %i[show update destroy]
 
   def index
-    @exercises = routine.exercises.all
+    @exercises = @routine.exercises.all
 
-    render json: serializer.new(@exercises)
+    render json: serializer.new(@exercises), status: :ok
   end
 
   def show
-    render json: serializer.new(@exercise)
+    render json: serializer.new(@exercise), status: :ok
   end
 
   def create
-    @exercise = routine.exercises.build(exercise_params)
+    @exercise = @routine.exercises.build(exercise_params)
+    @exercise.save!
 
-    if @exercise.save
-      render json: serializer.new(@exercise), status: :created
-    else
-      render json: @exercise.errors, status: :unprocessable_entity
-    end
+    render json: serializer.new(@exercise), status: :created
+  rescue ActiveRecord::RecordInvalid
+    render json: { errors: @exercise.errors }, status: :unprocessable_entity
   end
 
   def update
-    if @exercise.update(exercise_params)
-      render json: serializer.new(@exercise)
-    else
-      render json: @exercise.errors, status: :unprocessable_entity
-    end
+    @exercise.update!(exercise_params)
+
+    render json: serializer.new(@exercise), status: :ok
+  rescue ActiveRecord::RecordInvalid
+    render json: { errors: @exercise.errors }, status: :unprocessable_entity
   end
 
   def destroy
@@ -35,27 +35,21 @@ class ExercisesController < ApplicationController
   end
 
   def progress
-    @exercises = Exercise.joins(
-      "INNER JOIN routines
-        ON routines.id = exercises.routine_id
-        AND routines.user_id = #{current_user.id}
-        AND exercises.name = '#{params[:name]}'
-      ORDER BY routines.day ASC"
-    )
+    @exercise = Exercise.joins(:routine).where("routines.user_id = #{@current_user.id}
+      AND exercises.name = '#{params[:name]}'").order('routines.day ASC')
 
-    render json: serializer.new(@exercises)
+    render json: serializer.new(@exercises), status: :ok
   end
 
   private
 
   def set_exercise
-    @exercise = routine.exercises.find(params[:id])
+    @exercise = @routine.exercises.find(params[:id])
   end
 
   def exercise_params
     params
-      .require(:data)
-      .require(:attributes)
+      .require(:exercise)
       .permit(:name, :link, :sets, :reps, :rest, :tempo) || ApplicationController::Parameters.new
   end
 
@@ -64,6 +58,6 @@ class ExercisesController < ApplicationController
   end
 
   def routine
-    current_user.routines.find(params[:routine_id])
+    @routine = @current_user.routines.find(params[:routine_id])
   end
 end
--- app/models/exercise.rb
+++ app/models/exercise.rb
@@ -1,9 +1,9 @@
 class Exercise < ApplicationRecord
-  validates :name, presence: true
-  validates :sets, presence: true
-  validates :reps, presence: true
-  validates :rest, presence: true
-  validates :tempo, presence: true
+  validates :name, presence: true, allow_blank: false
+  validates :sets, presence: true, allow_blank: false
+  validates :reps, presence: true, allow_blank: false
+  validates :rest, presence: true, allow_blank: false
+  validates :tempo, presence: true, allow_blank: false
 
   belongs_to :routine
 end
