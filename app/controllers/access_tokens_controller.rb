4ba5a1d0a943eef1a5b5723934bcb1999bac0035
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-10-09 00:10:52 -0500

    change serializer for access tokens controller

--- app/controllers/access_tokens_controller.rb
+++ app/controllers/access_tokens_controller.rb
@@ -5,7 +5,7 @@ class AccessTokensController < ApplicationController
     authenticator = User::Authenticator.new(authentication_params.to_h.symbolize_keys)
     authenticator.perform
 
-    render json: AccessTokenSerializer.new(authenticator.access_token), status: :created
+    render json: authenticator.access_token, status: :created
   end
 
   def destroy
