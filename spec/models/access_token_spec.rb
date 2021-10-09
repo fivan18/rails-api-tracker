2074981c0143d9a8270a4954ab342c143c440185
Ivan Guzman <ivanguzman@Ivans-MacBook-Air.local>
2021-10-09 09:20:17 -0500

    add test to generate token once

--- spec/models/access_token_spec.rb
+++ spec/models/access_token_spec.rb
@@ -2,13 +2,20 @@ require 'rails_helper'
 
 RSpec.describe AccessToken, type: :model do
   describe 'new' do
+    let(:user) { create :user }
+
     it 'should have a token present after initialize' do
-      expect(AccessToken.new.token).to be_present
+      access_token = build :access_token, user:user
+      expect(access_token.token).to be_present
     end
 
     it 'should generate a token for the user' do
-      user = create :user
       expect { user.create_access_token }.to change { AccessToken.count }.by(1)
+    end
+
+    it 'should generate token once' do
+      access_token = user.create_access_token
+      expect(access_token.token).to eq(access_token.reload.token)
     end
   end
 end
